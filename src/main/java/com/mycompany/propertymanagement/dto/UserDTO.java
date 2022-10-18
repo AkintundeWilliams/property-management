4ad4d406a00be2477718ea2d01c94c759b2824b6
Williams <williamsakintunde@gmail.com>
2022-10-19 00:32:04 +0200

    implemented user registration functionality

---
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.mycompany.propertymanagement.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL) // so empty password is not returned after being omitted while converting from Entity back to DTO
+@JsonIgnoreProperties (ignoreUnknown = true)  //so empty password is not returned
+public class UserDTO {
+    private Long id;
+    private String ownerName;
+    private String ownerEmail;
+    private String phone;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
